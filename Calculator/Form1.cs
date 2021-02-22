7a47c4a079d7ff4bd9c722378d3db4e8048e95d1
Краснобаев Кирилл <kkirill1@outlook.com>
2021-02-22 21:02:27 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public interface IOperation
+    {
+        int calc();
+    }
+
+    public class Number : IOperation
+    {
+        public int num { get; set; }
+
+        public int calc()
+        {
+            return num;
+        }
+
+        public int Parcing(string Str)
+        {
+            return 0;
+        }
+    }
+
+    public class Summ : IOperation
+    {
+        public IOperation a { get; set; }
+        public IOperation b { get; set; }
+
+        public int calc()
+        {
+            return a.calc() + b.calc();
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Number Str = new Number();
+            int First_Numb = Str.Parcing(textBox1.Text);
+            Number First = new Number();
+            First.num = First_Numb;
+
+            Number Second = new Number();
+            Second.num = 5;
+
+            Summ summ = new Summ();
+            summ.a = First;
+            summ.b = Second;
+
+            Summ summ1 = new Summ();
+            summ1.a = summ;
+            summ1.b = Second;
+
+            summ1.calc();
+
+        }
+    }
+}

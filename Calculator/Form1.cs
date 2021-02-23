1e699e74bf584e717c37a6ebbbc1f717801577bd
Краснобаев Кирилл <kkirill1@outlook.com>
2021-02-23 10:49:18 +0300

    Add Parcing

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -24,10 +24,20 @@ namespace Calculator
             return num;
         }
 
+        public string Half_String { get; set; }
         public int Parcing(string Str)
         {
-            return 0;
+            string Value = "";
+            while (Str.Substring(0, 1) != "+") 
+            {
+                Value = Value + Str.Substring(0, 1);
+                Str = Str.Remove(0, 1);
+            }
+            Number H_String = new Number();
+            H_String.Half_String = Str.Remove(0, 1);
+            return Convert.ToInt32(Value);
         }
+
     }
 
     public class Summ : IOperation
@@ -37,6 +47,7 @@ namespace Calculator
 
         public int calc()
         {
+            //switch case для выбора знака, который и будем считать 
             return a.calc() + b.calc();
         }
     }
@@ -50,16 +61,20 @@ namespace Calculator
         private void button1_Click(object sender, EventArgs e)
         {
             Number Str = new Number();
+
             int First_Numb = Str.Parcing(textBox1.Text);
+            string Half_String = Str.Half_String;
             Number First = new Number();
             First.num = First_Numb;
 
             Number Second = new Number();
-            Second.num = 5;
+            int Second_Numb = Str.Parcing(Half_String);
+            Second.num = Second_Numb;
 
             Summ summ = new Summ();
             summ.a = First;
             summ.b = Second;
+            summ.calc();
 
             Summ summ1 = new Summ();
             summ1.a = summ;
